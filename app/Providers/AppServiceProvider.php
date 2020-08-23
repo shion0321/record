13e94e1a1c21dd3b98a3b49f0631890a49a8feb7
Shion Aoki <shio0321pc@gmail.com>
2020-08-24 01:13:12 +0900

    add

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use App\View\Components\TextArea;
+use Illuminate\Support\Facades\Blade;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -23,6 +26,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Blade::component('textarea', TextArea::class);
     }
 }
