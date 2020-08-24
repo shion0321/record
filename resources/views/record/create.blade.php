9a7a9dd0e7701d153ecb455fa883334eacef1211
Shion Aoki <shio0321pc@gmail.com>
2020-08-24 14:59:22 +0900

    add

--- resources/views/record/create.blade.php
+++ resources/views/record/create.blade.php
@@ -29,377 +29,48 @@
     </li>
   </ul>
 
-  <!-- 写真部分 -->
   <div class="tab-content">
+    <!-- 日足 -->
     <div id="oneday" class="tab-pane active">
-        <div>
-            <!-- 日足 -->
-            <div class="">
-                <label for="">
-                    日足のトレンド方向
-                </label>
-                <div class="form-check">
-                    <input class="form-check-input" type="radio" name="oneday_trend" id="oneday_trend_up" value="上昇トレンド">
-                    <label class="form-check-label" for="oneday_trend_up">上昇トレンド</label>
-                </div>
-                <div class="form-check">
-                    <input class="form-check-input" type="radio" name="oneday_trend" id="oneday_trend_down" value="下降トレンド">
-                    <label class="form-check-label" for="oneday_trend_down">下降トレンド</label>
-                </div>
-            </div>
-            <div class='form-group'>
-                <label for="">
-                    日足の画像
-                </label>
-                <input type="file" name="oneday_image_path" id="" class="form-control">
-            </div>
-            <!--  -->
-            <div class='form-group'>
-                <label for="">
-                    日足の値動きの流れ
-                </label>
-                <input type="text" name="oneday_flow" id="" class="form-control">
-            </div>
-            <!--  -->
-            <div class='form-group'>
-                <label for="">
-                    日足のエントリーポイント
-                </label>
-                <input type="text" name="oneday_entry_point" id="" class="form-control">
-            </div>
-            <!--  -->
-            <div class='form-group'>
-                <label for="">
-                    日足の利確位置
-                </label>
-                <input type="text" name="oneday_profit_point" id="" class="form-control">
-            </div>
-            <!--  -->
-            <div class='form-group'>
-                <label for="">
-                    日足の注意点
-                </label>
-                <input type="text" name="oneday_caution" id="" class="form-control">
-            </div>
-        </div>
+        <x-check-box label="日足のトレンド方向" name="oneday_trend" up-name="上昇トレンド" down-name="下降トレンド"/>
+        <x-image name="oneday_image_path" label="日足の画像"/>
+        <div class="preview"></div>
+        <x-textarea name="oneday_flow" label="日足の値動きの流れ"/>
+        <x-textarea name="oneday_entry_point" label="日足のエントリーポイント"/>
+        <x-textarea name="oneday_profit_point" label="日足の利確位置"/>
+        <x-textarea name="oneday_caution" label="日足の注意点"/>
     </div>
+    <!-- 4時間足 -->
     <div id="four_hours" class="tab-pane">
-        <div>
-            <div class='form-group'>
-                <label for="">
-                    4時間足のトレンド方向
-                </label>
-                <input type="text" name="four_hours_trend" id="" class="form-control">
-            </div>
-            <!--  -->
-            <div class='form-group'>
-                <label for="">
-                    4時間足の画像
-                </label>
-                <input type="file" name="four_hours_image_path" id="" class="form-control">
-            </div>
-            <!--  -->
-            <div class='form-group'>
-                <label for="">
-                    4時間足の値動きの流れ
-                </label>
-                <input type="text" name="four_hours_flow" id="" class="form-control">
-            </div>
-            <!--  -->
-            <div class='form-group'>
-                <label for="">
-                    4時間足のエントリーポイント
-                </label>
-                <input type="text" name="four_hours_entry_point" id="" class="form-control">
-            </div>
-            <!--  -->
-            <div class='form-group'>
-                <label for="">
-                    4時間足の利確位置
-                </label>
-                <input type="text" name="four_hours_profit_point" id="" class="form-control">
-            </div>
-            <!--  -->
-            <div class='form-group'>
-                <label for="">
-                    4時間足の注意点
-                </label>
-                <input type="text" name="four_hours_caution" id="" class="form-control">
-            </div>
-        </div>
+        <x-check-box name="four_hours_trend" label="4時間足のトレンド方向" up-name="上昇トレンド" down-name="下降トレンド"/>
+        <x-image name="four_hours_image_path" label="4時間足の画像"/>
+        <div class="preview"></div>
+        <x-textarea name="four_hours_flow" label="4時間足の値動きの流れ"/>
+        <x-textarea name="four_hours_entry_point" label="4時間足のエントリーポイント"/>
+        <x-textarea name="four_hours_profit_point" label="4時間足の利確位置"/>
+        <x-textarea name="four_hours_caution" label="4時間足の注意点"/>
     </div>
+    <!-- 1時間足 -->
     <div id="one_hour" class="tab-pane">
-        <div>
-            <div class='form-group'>
-        <label for="">
-            1時間足のトレンド方向
-        </label>
-        <input type="text" name="one_hour_trend" id="" class="form-control">
+        <x-check-box name="one_hour_trend" label="1時間足のトレンド方向" up-name="上昇トレンド" down-name="下降トレンド"/>
+        <x-image name="one_hour_image_path" label="1時間足の画像" />
+        <div class="preview"></div>
+        <x-textarea name="one_hour_flow" label="1時間足の値動きの流れ"/>
+        <x-textarea name="one_hour_entry_point" label="1時間足のエントリーポイント"/>
+        <x-textarea name="one_hour_profit_point" label="1時間足の利確位置"/>
+        <x-textarea name="one_hour_caution" label="1時間足の注意点"/>
     </div>
-    <!--  -->
-    <div class='form-group'>
-        <label for="">
-            1時間足の画像
-        </label>
-        <input type="file" name="one_hour_image_path" id="" class="form-control">
-    </div>
-    <!--  -->
-    <div class='form-group'>
-        <label for="">
-            1時間足の値動きの流れ
-        </label>
-        <input type="text" name="one_hour_flow" id="" class="form-control">
-    </div><!--  -->
-    <div class='form-group'>
-        <label for="">
-            1時間足のエントリーポイント
-        </label>
-        <input type="text" name="one_hour_entry_point" id="" class="form-control">
-    </div><!--  -->
-    <div class='form-group'>
-        <label for="">
-            1時間足の利確位置
-        </label>
-        <input type="text" name="one_hour_profit_point" id="" class="form-control">
-    </div>
-    <!--  -->
-    <div class='form-group'>
-        <label for="">
-            1時間足の注意点
-        </label>
-        <input type="text" name="one_hour_caution" id="" class="form-control">
-    </div>
-        </div>
-    </div>
+    <!-- 結果 -->
     <div id="result" class="tab-pane">
-       <div>
-            <div class='form-group'>
-        <label for="">
-            エントリ―時画像
-        </label>
-        <input type="file" name="entry_image_path" id="" class="form-control">
-    </div>
-    <!--  -->
-    <div class='form-group'>
-        <label for="">
-            決済時画像
-        </label>
-        <input type="file" name="finish_image_path" id="" class="form-control">
-    </div>
-    <!--  -->
-    <div class='form-group'>
-        <label for="">
-            結果
-        </label>
-        <input type="text" name="result" id="" class="form-control">
-    </div>
-    <!--  -->
-    <div class='form-group'>
-        <label for="">
-            獲得PIPS
-        </label>
-        <input type="text" name="result_pips" id="" class="form-control">
-    </div>
-    <!--  -->
-    <div class='form-group'>
-        <label for="">
-            獲得金額
-        </label>
-        <input type="text" name="result_profit" id="" class="form-control">
-    </div>
-    <!--  -->
-    <div class='form-group'>
-        <label for="">
-            振り返り
-        </label>
-        <input type="text" name="review" id="" class="form-control">
-    </div>
-       </div>
+        <x-image name="entry_image_path" label="エントリ―時画像" />
+        <div class="preview"></div>
+        <x-image name="finish_image_path" label="決済時画像" />
+        <x-check-box name="result" label="結果" up-name="利確" down-name="損切"/>
+        <x-text name="result_pips" label="獲得PIPS"/>
+        <x-text name="result_profit" label="獲得金額"/>
+        <x-textarea name="review" label="振り返り"/>
     </div>
   </div>
 
-    <!--  -->
-    {{-- <div>
-        <label for="">
-        </label>
-        <input type="text" name="" id="">
-    </div> --}}
-    <!-- 通貨ペア -->
-    {{-- <div class='form-group'>
-        <label for="">
-            通貨ペア
-        </label>
-        <input type="text" name="currency_pair" id="" class="form-control">
-    </div> --}}
-    {{-- <!-- 日足 -->
-    <div class='form-group'>
-        <label for="">
-            日足のトレンド方向
-        </label>
-        <input type="text" name="oneday_trend" id="" class="form-control">
-    </div>
-    <!--  -->
-    <div class='form-group'>
-        <label for="">
-            日足の画像
-        </label>
-        <input type="file" name="oneday_image_path" id="" class="form-control">
-    </div>
-    <!--  -->
-    <div class='form-group'>
-        <label for="">
-            日足の値動きの流れ
-        </label>
-        <input type="text" name="oneday_flow" id="" class="form-control">
-    </div>
-    <!--  -->
-    <div class='form-group'>
-        <label for="">
-            日足のエントリーポイント
-        </label>
-        <input type="text" name="oneday_entry_point" id="" class="form-control">
-    </div>
-    <!--  -->
-    <div class='form-group'>
-        <label for="">
-            日足の利確位置
-        </label>
-        <input type="text" name="oneday_profit_point" id="" class="form-control">
-    </div>
-    <!--  -->
-    <div class='form-group'>
-        <label for="">
-            日足の注意点
-        </label>
-        <input type="text" name="oneday_caution" id="" class="form-control">
-    </div> --}}
-    <!--  -->
-    {{-- <div class='form-group'>
-        <label for="">
-            4時間足のトレンド方向
-        </label>
-        <input type="text" name="four_hours_trend" id="" class="form-control">
-    </div>
-    <!--  -->
-    <div class='form-group'>
-        <label for="">
-            4時間足の画像
-        </label>
-        <input type="file" name="four_hours_image_path" id="" class="form-control">
-    </div>
-    <!--  -->
-    <div class='form-group'>
-        <label for="">
-            4時間足の値動きの流れ
-        </label>
-        <input type="text" name="four_hours_flow" id="" class="form-control">
-    </div>
-    <!--  -->
-    <div class='form-group'>
-        <label for="">
-            4時間足のエントリーポイント
-        </label>
-        <input type="text" name="four_hours_entry_point" id="" class="form-control">
-    </div>
-    <!--  -->
-    <div class='form-group'>
-        <label for="">
-            4時間足の利確位置
-        </label>
-        <input type="text" name="four_hours_profit_point" id="" class="form-control">
-    </div>
-    <!--  -->
-    <div class='form-group'>
-        <label for="">
-            4時間足の注意点
-        </label>
-        <input type="text" name="four_hours_caution" id="" class="form-control">
-    </div> --}}
-    <!--  -->
-    {{-- <div class='form-group'>
-        <label for="">
-            1時間足のトレンド方向
-        </label>
-        <input type="text" name="one_hour_trend" id="" class="form-control">
-    </div>
-    <!--  -->
-    <div class='form-group'>
-        <label for="">
-            1時間足の画像
-        </label>
-        <input type="file" name="one_hour_image_path" id="" class="form-control">
-    </div>
-    <!--  -->
-    <div class='form-group'>
-        <label for="">
-            1時間足の値動きの流れ
-        </label>
-        <input type="text" name="one_hour_flow" id="" class="form-control">
-    </div><!--  -->
-    <div class='form-group'>
-        <label for="">
-            1時間足のエントリーポイント
-        </label>
-        <input type="text" name="one_hour_entry_point" id="" class="form-control">
-    </div><!--  -->
-    <div class='form-group'>
-        <label for="">
-            1時間足の利確位置
-        </label>
-        <input type="text" name="one_hour_profit_point" id="" class="form-control">
-    </div>
-    <!--  -->
-    <div class='form-group'>
-        <label for="">
-            1時間足の注意点
-        </label>
-        <input type="text" name="one_hour_caution" id="" class="form-control">
-    </div> --}}
-    <!--  -->
-    {{-- <div class='form-group'>
-        <label for="">
-            エントリ―時画像
-        </label>
-        <input type="file" name="entry_image_path" id="" class="form-control">
-    </div>
-    <!--  -->
-    <div class='form-group'>
-        <label for="">
-            決済時画像
-        </label>
-        <input type="file" name="finish_image_path" id="" class="form-control">
-    </div>
-    <!--  -->
-    <div class='form-group'>
-        <label for="">
-            結果
-        </label>
-        <input type="text" name="result" id="" class="form-control">
-    </div>
-    <!--  -->
-    <div class='form-group'>
-        <label for="">
-            獲得PIPS
-        </label>
-        <input type="text" name="result_pips" id="" class="form-control">
-    </div>
-    <!--  -->
-    <div class='form-group'>
-        <label for="">
-            獲得金額
-        </label>
-        <input type="text" name="result_profit" id="" class="form-control">
-    </div>
-    <!--  -->
-    <div class='form-group'>
-        <label for="">
-            振り返り
-        </label>
-        <input type="text" name="review" id="" class="form-control">
-    </div> --}}
-
-    <div>
-        <button>登録</button>
-    </div>
 </form>
 @endsection
