bdf6a9a58502397123e3178ed232acff046ba480
Shion Aoki <shio0321pc@gmail.com>
2020-08-28 00:07:49 +0900

    add

---
+++ resources/views/components/textarea.blade.php
@@ -0,0 +1,6 @@
+<div class='form-group'>
+    <label for="{{ $name }}">
+        {{ $label }}
+    </label>
+    <textarea name="{{ $name }}" id="{{ $name }}" class="form-control"></textarea>
+</div>
