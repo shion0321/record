5d171379e7ccdad39010d019cc752c4addb3423f
Shion Aoki <shio0321pc@gmail.com>
2020-09-13 22:59:12 +0900

    add

--- resources/views/components/image.blade.php
+++ resources/views/components/image.blade.php
@@ -2,5 +2,8 @@
     <label for="{{ $name }}">
         {{ $label }}
     </label>
+    <div>
+        <img src="{{ $record ?? '' }}" alt="" class="w-100">
+    </div>
     <input type="file" name="{{ $name }}" id="{{ $name }}" class="form-control">
 </div>
