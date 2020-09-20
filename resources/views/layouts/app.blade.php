2304022f1b39426e2c94fd5d843b3e05d06ecb9f
Shion Aoki <shio0321pc@gmail.com>
2020-09-20 15:59:03 +0900

    faviconの追加

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="shortcut icon" href="{{ asset('/record.png') }}">
 </head>
 <body>
     <div id="app">
