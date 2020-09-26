836ea257f88b8cfde4fc2d791cdcc2a411f9f3d8
Shion Aoki <shio0321pc@gmail.com>
2020-09-26 16:31:08 +0900

    oldセット

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -3,7 +3,7 @@
     <label for="" class="font-weight-bold">{{ $label }}</label>
     <select id="" class="form-control" name="{{ $name }}">
         @foreach ($records as $key => $value)
-            <option value="{{ $key }}">{{ $value }}</option>
+            <option value="{{ $key }}" {{ old($name) == $key ? 'selected' : ''}}>{{ $value }}</option>
         @endforeach
     </select>
 </div>
